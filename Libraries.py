576d5d6ed8272ed3cb7f7513afd640f276127a30
prabhaschepuri <prabhaschepuri@gmail.com>
2026-06-20 00:27:05 +0530

    Add essential libraries for data processing and ML

---
+++ Libraries.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import cv2 as cv
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import Sequential
+from tensorflow.keras import layers, models
+from tensorflow.keras.layers import Dense
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.layers import GlobalAveragePooling2D
